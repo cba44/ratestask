08b0f38b403c3c1d8b72b2dc0ec497f62a5161e7
cba44 <cba_44@yahoo.com>
2024-05-02 22:40:38 +0200

    fix: Fising database connection close logic

--- app/db/rates.py
+++ app/db/rates.py
@@ -8,53 +8,62 @@ class RatesDb:
         cur = conn.cursor()
         try:
             cur.execute(RatesQuery.subregion_query, {'slug': slug})
+            regions = []
+            for region in cur.fetchall():
+                regions.append(region[0])
+            regions_tuple = tuple(regions)
+            return regions_tuple
         except:
             raise ValueError(f"Invalid region name for {location_name}")
-        regions = []
-        for region in cur.fetchall():
-            regions.append(region[0])
-        regions_tuple = tuple(regions)
-        cur.close()
-        conn.close()
-        return regions_tuple
+        finally:
+            cur.close()
+            conn.close()
+        
     
     def get_port_count(port, location_name):
         conn = Utils.get_db_connection()
         cur = conn.cursor()
-
         try:
             cur.execute(RatesQuery.port_count_query, {'port': port})
+            port_count = cur.fetchone()[0]
+            return port_count
         except:
             raise ValueError(f"Invalid port name for {location_name}")
-
-        port_count = cur.fetchone()[0]
-        cur.close()
-        conn.close()
-        return port_count
+        finally:
+            cur.close()
+            conn.close()
+        
     
     def get_all_ports(subregions, location_name):
         conn = Utils.get_db_connection()
         cur = conn.cursor()
         try:
             cur.execute(RatesQuery.subregion_ports_query, {'region': subregions})
+            ports = []
+            for port in cur.fetchall():
+                ports.append(port[0])
+            ports_tuple = tuple(ports)
+            return ports_tuple
         except:
             raise ValueError(f"Invalid region name for {location_name}")
-        ports = []
-        for port in cur.fetchall():
-            ports.append(port[0])
-        ports_tuple = tuple(ports)
-        cur.close()
-        conn.close()
-        return ports_tuple
+        finally:
+            cur.close()
+            conn.close()
+        
 
     def get_prices(date_from, date_to, origin_ports, destination_ports):
         conn = Utils.get_db_connection()
         cur = conn.cursor()
-        cur.execute(RatesQuery.prices_query, {'orig_codes': origin_ports,
+        try:
+            cur.execute(RatesQuery.prices_query, {'orig_codes': origin_ports,
                                               'dest_codes': destination_ports,
                                               'start_day': date_from,
                                               'end_day': date_to})
-        prices = cur.fetchall()
-        cur.close()
-        conn.close()
-        return prices
+            prices = cur.fetchall()
+            return prices
+        except:
+            raise ValueError("Invalid value entered")
+        finally:
+            cur.close()
+            conn.close()
+        
